702bf33358f04584ffdfe73391b13838ec5e1dcc
ethanabrooks <ethanabrooks@gmail.com>
2025-08-07 19:13:43 -0400

    filter `model_content` with empty `parts` arrays`

--- pydantic_ai_slim/pydantic_ai/models/google.py
+++ pydantic_ai_slim/pydantic_ai/models/google.py
@@ -382,7 +382,10 @@ class GoogleModel(Model):
                     message_parts = [{'text': ''}]
                 contents.append({'role': 'user', 'parts': message_parts})
             elif isinstance(m, ModelResponse):
-                contents.append(_content_model_response(m))
+                model_content = _content_model_response(m)
+                # Filter out model responses with empty parts as they cause API errors
+                if model_content.get('parts'):
+                    contents.append(model_content)
             else:
                 assert_never(m)
         if instructions := self._get_instructions(messages):
